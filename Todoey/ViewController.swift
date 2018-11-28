c7f06af483d057bccc8dfde3a1cd8f0d7d89d0fd
Derrick Lin <derricklin@derricks-macbook-pro-2.local>
2018-11-28 08:29:50 -0800

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Derrick Lin on 11/28/18.
+//  Copyright © 2018 Derrick Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
