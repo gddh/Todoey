a1cdb2f43227004d5f42d59c34bcd2bc789c49e2
Derrick Lin <derricklin@derricks-macbook-pro-2.local>
2018-11-30 09:33:14 -0800

    Convert to MVC Design Pattern

---
+++ Todoey/Data Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Derrick Lin on 11/30/18.
+//  Copyright © 2018 Derrick Lin. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
